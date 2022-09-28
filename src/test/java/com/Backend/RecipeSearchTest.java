e7099eef39b3824e397e8c6041227e0e6f0060e6
filipE <fille_900@live.se>
2022-09-28 13:24:59 +0200

    La till getMatchingPercentage som metod och la även till ett test för denna

--- src/test/java/com/Backend/RecipeSearchTest.java
+++ src/test/java/com/Backend/RecipeSearchTest.java
@@ -54,5 +54,36 @@ public class RecipeSearchTest {
     }
 
 
+    @Test
+    public void getMatchingPercentageTest() {
+        RecipeSearch recipeSearch = new RecipeSearch();
+
+        Ingredient butter = new Ingredient(1, "Butter");
+        Ingredient milk = new Ingredient(2, "Milk");
+        Ingredient salt = new Ingredient(3, "Salt");
+        Ingredient sugar = new Ingredient(4, "Sugar");
+        Ingredient flour = new Ingredient(5, "Flour");
+        List<Ingredient> recipeIngredients = new ArrayList<Ingredient>();
+        recipeIngredients.add(butter);
+        recipeIngredients.add(milk);
+        recipeIngredients.add(salt);
+        List<Ingredient> storageIngredients = new ArrayList<Ingredient>();
+        storageIngredients.add(salt);
+        storageIngredients.add(sugar);
+        storageIngredients.add(flour);
+
+        int expectedPercentage = 33;
+
+        assertEquals(recipeSearch.getMatchingPercentage(storageIngredients, recipeIngredients), expectedPercentage);
+
+
+
+    }
+
+
+
+
+
+
 
 }
