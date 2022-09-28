598fca94949eb366fd17a583163d6d8d506a88a8
rikard <rikardroos@hotmail.com>
2022-09-28 11:32:05 +0200

    Singleton Database class

--- src/main/java/com/grupp12/grupp12projekt/backend/Database.java
+++ src/main/java/com/grupp12/grupp12projekt/backend/Database.java
@@ -1,5 +1,24 @@
 package com.grupp12.grupp12projekt.backend;
 
+import java.util.ArrayList;
+
 public class Database {
-    
+    ArrayList<Recipe> allRecipes;
+    private static final Database instance = new Database();
+
+    private Database() {
+        //Privat konstruktor
+    }
+
+    public static Database getInstance() {
+        return Database.instance;
+    }
+
+    ArrayList<Recipe> getAllRecipes() {
+        return allRecipes;
+    }
+
+    void addRecipe(Recipe recipe) {
+        allRecipes.add(recipe);
+    }
 }
