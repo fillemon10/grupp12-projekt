43cb3605065d19a67f517ddc4b87420265251f38
rikard <rikardroos@hotmail.com>
2022-10-07 10:36:16 +0200

    GUI first version

--- src/main/java/com/grupp12/grupp12projekt/App2good2go.java
+++ src/main/java/com/grupp12/grupp12projekt/App2good2go.java
@@ -1,5 +1,9 @@
 package com.grupp12.grupp12projekt;
 
+import com.grupp12.grupp12projekt.Controller.HomePageController;
+import com.grupp12.grupp12projekt.Controller.NavigationbarController;
+import com.grupp12.grupp12projekt.Controller.StorageController;
+import com.grupp12.grupp12projekt.backend.Storage;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,9 +15,10 @@ public class App2good2go extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App2good2go.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(App2good2go.class.getResource("root-page.fxml"));
+        fxmlLoader.setController(NavigationbarController.getInstance());
+        Scene scene = new Scene(fxmlLoader.load(), 780 ,500);
+        stage.setTitle("2good2go");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/grupp12/grupp12projekt/Controller/StorageController.java
+++ src/main/java/com/grupp12/grupp12projekt/Controller/StorageController.java
@@ -1,5 +1,33 @@
 package com.grupp12.grupp12projekt.Controller;
 
-public class StorageController implements IController {
+import com.grupp12.grupp12projekt.App2good2go;
+import com.grupp12.grupp12projekt.Model;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
+
+public class StorageController extends AnchorPane implements IController {
+    Model model;
+    private static StorageController instance;
+
+    public static StorageController getInstance() {
+        if (instance == null)
+            instance = new StorageController();
+        return instance;
+    }
+
+    private StorageController() {
+        model = Model.getInstance();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(App2good2go.class.getResource("storage-view.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
 }
